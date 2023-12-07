efc6187149fc0258c19ee170fe13abda7b3a9e28
alex <alexandrosrotsidis@icloud.com>
2023-12-07 20:41:50 +0200

    wip

--- src/examples/libvot_feature.cpp
+++ src/examples/libvot_feature.cpp
@@ -58,6 +58,7 @@ extern "C" {
 }
 
 #ifdef LIBVOT_USE_OPENCV
+#include "opencv2/opencv.hpp"
 #include <opencv2/core/core.hpp>
 // #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/features2d.hpp>
