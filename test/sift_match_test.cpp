c86bc2fb49292127ddcd7d24c2fea062d8753c88
alex <alexandrosrotsidis@icloud.com>
2023-12-07 20:44:06 +0200

    wip

--- test/sift_match_test.cpp
+++ test/sift_match_test.cpp
@@ -13,6 +13,7 @@
 
 #ifdef LIBVOT_USE_OPENCV
 #include "opencv2/core/core.hpp"
+#include "opencv2/opencv.hpp"
 #include "opencv2/features2d/features2d.hpp"
 // #include "opencv2/highgui/highgui.hpp"
 #include "opencv2/features2d.hpp"
